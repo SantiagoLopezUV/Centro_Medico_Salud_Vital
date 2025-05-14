948ace9c0ce8e16e835cd05703924f8213ba9e07
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-05-13 21:58:32 -0500

    Creacion Repositorio Proyecto Final Base de Datos

---
+++ README.md
@@ -0,0 +1,8 @@
+# Centro Médico Salud Vital
+
+Readme, presentacion Proyecto
+
+Java 17
+
+Proyecto en lenguaje Java para gestión de pacientes y citas del centro medico 'Salud Vital'.
+
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Desarrollo del Proyecto Final De la Materia Bases De Datos
+        System.out.println("Hello world!");
+    }
+}
