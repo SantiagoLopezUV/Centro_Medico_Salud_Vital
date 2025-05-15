7dd422f1e0c5e8eab24bdcc97cc3dfb7e5b5d78e
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-05-15 08:04:38 -0500

    Creacion de clase Factura, Setter and Getters

---
+++ src/model/Factura.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Factura {
+
+    private int invoiceNumber;  //   refFactura
+    private LocalDate date; //  fecha
+    private LocalTime time; //  hora
+    private double total;   //  valorTotal
+
+    public Factura(int invoiceNumber, LocalDate date, LocalTime time, double total) {
+        this.invoiceNumber = invoiceNumber;
+        this.date = date;
+        this.time = time;
+        this.total = total;
+    }
+
+    public int getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(int invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total += total;
+    }
+
+}
