7412c75215d47ea9c28f231f96f852026d5dea74
Juan D <juan-12_91@hotmail.com>
2025-05-15 09:02:47 -0500

    appointment modification started

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,29 +1,57 @@
 package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 //  Clase de la entidad 'Cita'
 public class Appointment {
 
     private int IdAppointment;
-    private String status;
+    private AppointmentStatus status;
+    private Long patientId;
+    private Long medicId;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    private int consultationId;
+    private double consultationRegisteredPrice;
+    private int invoiceNumber;
+    private int arrangementCode;
+    private double arrangementDiscountApplied;
 
-    public Appointment(int idAppointment, String status) {
+    public Appointment(int idAppointment,
+                       AppointmentStatus status,
+                       Long patientId,
+                       Long medicId,
+                       LocalDate appointmentDate,
+                       LocalTime appointmentTime,
+                       int consultationId,
+                       double consultationRegisteredPrice,
+                       int arrangementCode,
+                       double arrangementDiscountApplied) {
+        if(idAppointment < 0
+                || status == null
+                || patientId == null
+                || patientId < 99999
+                || medicId == null
+                || medicId < 99999
+                || appointmentDate == null
+                || appointmentDate.isBefore(LocalDate.now())
+                || appointmentTime == null
+                || appointmentTime.isBefore(LocalTime.now())
+                || consultationId < 0
+                || consultationRegisteredPrice < 0) {
+
+        }
+
         IdAppointment = idAppointment;
         this.status = status;
+        this.patientId = patientId;
+        this.medicId = medicId;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.consultationId = consultationId;
+        this.consultationRegisteredPrice = consultationRegisteredPrice;
+        this.arrangementCode = arrangementCode;
+        this.arrangementDiscountApplied = arrangementDiscountApplied;
     }
-
-    public int getIdAppointment() {
-        return IdAppointment;
-    }
-
-    public void setIdAppointment(int idAppointment) {
-        IdAppointment = idAppointment;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    
 }
