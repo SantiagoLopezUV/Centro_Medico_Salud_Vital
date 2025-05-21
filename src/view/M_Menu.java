7fd58878a57c12e084e30df2cff5c3c4af78f2e3
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-05-21 01:27:46 -0500

    Creation form for Manager

---
+++ src/view/M_Menu.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class M_Menu {
+    private JPanel M_MenuPanel;
+    private JButton M_Menu_UpdateCostsBttn;
+    private JButton M_Menu_DiscountsAndBenefitsBttn;
+    private JLabel M_ManagerTitle;
+    private JButton M_Menu_ReturnBttn;
+    private JLabel M_Menu_DocsIcon;
+}
