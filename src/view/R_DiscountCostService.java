6183ebbf9d0845ef15d1850b727c34493a331f6e
juanjo380 <juan.bolanos.serna@correounivalle.edu.co>
2025-05-22 17:11:50 -0500

    The last one form was added

---
+++ src/view/R_DiscountCostService.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+
+public class R_DiscountCostService {
+    private JPanel R_DiscountCostServicePanel;
+    private JComboBox R_AgreementComboBox;
+    private JComboBox R_ServiceComboBox;
+    private JFormattedTextField R_ValueFormattedTextField;
+    private JLabel R_Menu_ConsultationRecepcionistTitle;
+    private JLabel R_Menu_Consultation_SubTitleRecep;
+    private JLabel R_DiscountCostService_ServiceLabel;
+    private JLabel R_DiscountCostService_AgreementLabel;
+    private JLabel R_DiscountCostService_ValueLabel;
+}
