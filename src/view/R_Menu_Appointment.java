76acaf4cac85ddb6d4f963d7b5c2ea5549876a14
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-05-22 12:53:31 -0500

    Add AccionListener from the forms Menus

--- src/view/R_Menu_Appointment.java
+++ src/view/R_Menu_Appointment.java
@@ -3,6 +3,8 @@ package view;
 import utils.AccessPanel;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class R_Menu_Appointment implements AccessPanel {
     private JPanel R_Menu_AppointmentPanel;
@@ -12,6 +14,28 @@ public class R_Menu_Appointment implements AccessPanel {
     private JButton R_Menu_Appointment_AppointmentFulfillmentBttn;
     private JButton R_Menu_Appointment_ReturnBttn;
     private JLabel R_Menu_Appointment_ScheduleIcon;
+
+    public R_Menu_Appointment() {
+        R_Menu_Appointment_ReturnBttn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AccessPanel.changeContent("R_Menu");
+            }
+        });
+        R_Menu_Appointment_ScheduleAppointmentBttn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AccessPanel.changeContent("R_ScheduleAppointment");
+            }
+        });
+
+        R_Menu_Appointment_AppointmentFulfillmentBttn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AccessPanel.changeContent("R_AppointmentFulfillment");
+            }
+        });
+    }
 
     @Override
     public JPanel getPanel() {
