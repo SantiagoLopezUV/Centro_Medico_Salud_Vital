93153e0257bf22318b5c1c7d3e052d2d14b55fa1
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2025-05-29 20:22:46 -0500

    Correction on Patient Class, arrangement field added

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,14 +1,24 @@
 package model;
 
 public class Patient extends Person{
-
+    private Arrangement arrangement;
     public Patient(long id,
                    String firstMiddleName,
                    String lastName,
                    String telephoneNumber,
                    String email,
                    String address,
-                   SexType sex) {
+                   SexType sex,
+                   Arrangement arrangement) {
         super(id, firstMiddleName, lastName, telephoneNumber, email, address, sex);
+        this.arrangement = arrangement;
+    }
+
+    public Arrangement getArrangement() {
+        return arrangement;
+    }
+
+    public void setArrangement(Arrangement arrangement) {
+        this.arrangement = arrangement;
     }
 }
