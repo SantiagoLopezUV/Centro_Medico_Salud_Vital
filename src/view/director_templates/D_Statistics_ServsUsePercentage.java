5737abc3fd042c0806ce7abf8052b0d668ab35e2
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2025-06-12 21:29:06 -0500

    the way to loging was changed, all the resonsibility goes to the db, also MathUtils class was created, and statistics additional service use percentaje service build begin

--- src/view/director_templates/D_Statistics_ServsUsePercentage.java
+++ src/view/director_templates/D_Statistics_ServsUsePercentage.java
@@ -1,10 +1,14 @@
 package view.director_templates;
 
+import dao.DirectorDao;
 import utils.AccessPanel;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class D_Statistics_ServsUsePercentage implements AccessPanel {
     private JPanel D_ServUsePercentageBG;
@@ -19,9 +23,11 @@ public class D_Statistics_ServsUsePercentage implements AccessPanel {
     private JLabel D_ServUsePercentage_SubTitleDir;
 
     public D_Statistics_ServsUsePercentage() {
-        this.D_ServUsePercentage_ReturnBttn.addActionListener(e ->
-                AccessPanel.changeContent("D_statistics"));
-        initTables();
+        this.D_ServUsePercentage_ReturnBttn.addActionListener(e -> {
+                AccessPanel.changeContent("D_statistics");
+                destroyData();
+        });
+        this.D_ServUsePercentage_SearchMonthBttn.addActionListener(e -> initTables());
         establishComboBoxesMonthYearValues(D_ServUsePercentage_comboBoxMonth, D_ServUsePercentage_comboBoxYear);
         this.D_ServUsePercentage_TotalTextField.setText("Total uso de servicios");
     }
@@ -31,20 +37,46 @@ public class D_Statistics_ServsUsePercentage implements AccessPanel {
         return this.D_ServUsePercentageBG;
     }
 
-    //Se debe modificar para cuando se hagam las respectivas querys
     private void initTables(){
 
-        //----reemplazar por las listas obtenias por las respectivas consultas
-        //----para cada tabla
-        Object[][] data= {
-                {"Rayos X", 0.10},
-                {"Pruebas de sangre",0.40},
-                {"Ortopedia", 0.0},
-                {"Electrocardiograma", 0.20},
-                {"Fisioterapia", 0.70}
-        };
+        DirectorDao directorDao = new DirectorDao();
+        Object[][] data =null;
+        String year;
+        int month;
+        double income = 0;
 
-        //----
+
+        try {
+
+            if(Objects.requireNonNull(this.D_MonthlyServIncome_comboBoxYear.getSelectedItem()).toString().isBlank()
+                    || Objects.requireNonNull(this.D_MonthlyServIncome_comboBoxMonth.getSelectedItem()).toString().isBlank())
+                throw new NoSuchFieldException();
+
+            year = this.D_MonthlyServIncome_comboBoxYear.getSelectedItem().toString();
+            month = this.D_MonthlyServIncome_comboBoxMonth.getSelectedIndex();
+
+
+            data = directorDao.getMonthlyAdditionalServsIncome(year, month);
+
+
+
+            for(int i=0; i<data.length; i++){
+                income = income + (double) data[i][3];
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                    "Un error se ha presentado al tratar de consultar la base de datos," +
+                            "contacta al administrador de la misma",
+                    "Error al consultar la base de datos",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (NoSuchFieldException e){
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                    "Verifica que los campos año y mes estén correctos",
+                    "Error al consultar la base de datos",
+                    JOptionPane.ERROR_MESSAGE);
+        }
         // table heads
         String[] columnNames = {"Servicio adicional","% uso vs el total"};
         // adding into to table
@@ -60,8 +92,13 @@ public class D_Statistics_ServsUsePercentage implements AccessPanel {
         this.D_ServUsePercentage_ScrollPanel.setMaximumSize(new Dimension(maxWidth,
                 maxHeight));
         resizeColumnsTable(addServsPercentageTable, maxWidth);
-
-
-
     }
+
+    private void destroyData() {
+        this.D_ServUsePercentage_TotalTextField.setText("Total uso de servicios");
+        this.D_ServUsePercentage_comboBoxMonth.setSelectedIndex(0);
+        this.D_ServUsePercentage_comboBoxYear.setSelectedIndex(0);
+        this.addServsPercentageTable.setModel(new DefaultTableModel());
+    }
+
 }
