9bbb96502216c4629fc850318230151bd44837f1
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-07-02 21:24:09 -0500

    Creation of Button to Calculate Price of the Type Consults

--- src/dao/ReceptionistDao.java
+++ src/dao/ReceptionistDao.java
@@ -3,7 +3,6 @@ package dao;
 import model.*;
 import utils.db.ConnectionSource;
 
-import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -34,11 +33,11 @@ public class ReceptionistDao {
 
     private static final String OUTSTANDING_DEBTS_EXIST_FOR_PATIENT_ID = "select * from cita where pacienteid = ? AND estado = 'Pendiente por pago';";
 
-    private static final String GET_STATUS_FOR_PATIENT_ID = "SELECT pacienteid, estado, costoconsreg FROM cita WHERE pacienteid = ? AND estado = 'Pendiente por pago';";
+    private static final String GET_PENDING_PAYMENT_FOR_PATIENT_ID = "SELECT pacienteid, estado, costoconsreg FROM cita WHERE pacienteid = ? AND estado = 'Pendiente por pago';";
 
 
 
-    public ArrayList<ConsultationType> getConsultationTypes() throws SQLException {
+    public static ArrayList<ConsultationType> getConsultationTypes() throws SQLException {
         ArrayList<ConsultationType> consultationTypes = new ArrayList<>();
         try(Connection conn = ConnectionSource.getConnection()) {
             try (Statement statement = conn.createStatement()) {
@@ -224,7 +223,7 @@ public class ReceptionistDao {
 
     public static infoDebts consultsDebts(long patientId) throws SQLException {
         try(Connection con = ConnectionSource.getConnection();) {
-            try (PreparedStatement ps = con.prepareStatement(GET_STATUS_FOR_PATIENT_ID)){
+            try (PreparedStatement ps = con.prepareStatement(GET_PENDING_PAYMENT_FOR_PATIENT_ID)){
                 ps.setLong(1, patientId);
                 try(ResultSet rs = ps.executeQuery()) {
                     if(rs.next()){
@@ -241,7 +240,7 @@ public class ReceptionistDao {
 
     public static boolean pendingDebts(long patientId) throws SQLException {
         try(Connection con = ConnectionSource.getConnection();) {
-            try (PreparedStatement ps = con.prepareStatement(GET_STATUS_FOR_PATIENT_ID)){
+            try (PreparedStatement ps = con.prepareStatement(GET_PENDING_PAYMENT_FOR_PATIENT_ID)){
                 ps.setLong(1, patientId);
                 try(ResultSet rs = ps.executeQuery()) {
                     return rs.next();
--- src/view/receptionist_templates/R_CostService.java
+++ src/view/receptionist_templates/R_CostService.java
@@ -1,9 +1,14 @@
 package view.receptionist_templates;
 
+import dao.ManagerDao;
+import dao.ReceptionistDao;
+import model.ConsultationType;
 import utils.AccessPanel;
 import utils.PlaceHoldersAction;
 
 import javax.swing.*;
+import java.sql.SQLException;
+import java.util.List;
 
 public class R_CostService implements AccessPanel {
     private JPanel R_CostServicePanel;
@@ -14,10 +19,37 @@ public class R_CostService implements AccessPanel {
     private JLabel R_CostService_lblValues;
     private JFormattedTextField R_CostService_IdPatientField;
     private JButton R_CostService_CalculateBttn;
-    private JList list1;
+    private JList<ConsultationType> list1;
     private JList list2;
+    private JScrollPane scroll1;
+    private JScrollPane scroll2;
 
     public R_CostService() {
+
+        list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        list1.setVisibleRowCount(8);
+        list2.setVisibleRowCount(8);
+
+        this.R_CostService_CalculateBttn.addActionListener(e -> {
+            try{
+                List<ConsultationType> listTypeConsultations = ReceptionistDao.getConsultationTypes();
+
+                DefaultListModel<ConsultationType> listModelNames = new DefaultListModel<>();
+                DefaultListModel<String> listModelValues = new DefaultListModel<>();
+
+                for (ConsultationType TypeConsultation : listTypeConsultations) {
+                    listModelNames.addElement(TypeConsultation);
+                    listModelValues.addElement(String.valueOf(TypeConsultation.getConsultationPrice()));
+                }
+                list1.setModel(listModelNames);
+                list2.setModel(listModelValues);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(this.R_CostServicePanel, "Error al Cargar Información");
+            }
+        });
+
 
         this.R_CostService_ReturnBttn.addActionListener(e ->
                 AccessPanel.changeContent("R_Menu_Consultation"));
